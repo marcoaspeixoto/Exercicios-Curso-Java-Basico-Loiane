851c9b4bbc6db280d668cbf2ff50133c94cf9a2c
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-10-26 16:40:37 -0300

    Exercicio 01

---
+++ CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio01.java
@@ -0,0 +1,11 @@
+// Faça um programa que mostre a mensagem "Alô mundo" na tela.
+
+package exercicios.aula13;
+
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Alô Mundo!");
+    }
+}
