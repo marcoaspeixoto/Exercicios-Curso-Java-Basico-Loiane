d871dd58b6a1eaa2849248bcae569bbe05886d74
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-10-29 23:46:14 -0300

    Exercicio 12

--- CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio10.java
+++ CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio10.java
@@ -1,6 +1,7 @@
 /*Faça um programa que peça a temperatura em graus Celsius, transforme e mostre
  *em graus Farenheit.
  */
+
 package exercicios.aula13;
 
 import java.util.Scanner;
---
+++ CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio12.java
@@ -0,0 +1,25 @@
+/* tendo como dados de entrada a altura de uma pessoa, construa um algoritmo
+ * que calcule seu peso ideal, usando a seguinte fórmula:
+ * (72.7 * altura) - 58
+ */
+
+package exercicios.aula13;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        float altura, pesoIdeal;
+        
+        System.out.print("Informe sua altura em metros: ");
+        altura = entrada.nextFloat();
+        
+        pesoIdeal = (72.7f * altura) - 58f;
+        
+        System.out.printf("Seu peso ideal: %.1f kg%n", pesoIdeal);
+    }
+}
