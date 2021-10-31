c71a4de17ae909643ffaa1f953cc7557ef73969b
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-10-30 22:21:53 -0300

    Exercicio 13

--- CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio13.java
+++ CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio13.java
@@ -1,14 +1,14 @@
 /* Faça um programa que pergunte quanto você ganha por hora e o número de horas
-* trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
-* sabendo-se que são descontados 11% para o imposto de renda, 8% para o INSS e 
-* 5% para o sindicato, faça um programa que nos dê:
-* a) salário bruto.
-* b) quanto pagou ao INSS.
-* c) quanto pagou ao sindicato.
-* d) calcule os descontos e o salário líquido, conforme a tabela abaixo:
-*
-* + Salário Bruto: R$ - IR (11%): R$ - INSS (8%): R$ - Sindicato (5%): R$ = 
-* Salário Líquido: R$
+ * trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
+ * sabendo-se que são descontados 11% para o imposto de renda, 8% para o INSS e 
+ * 5% para o sindicato, faça um programa que nos dê:
+ * a) salário bruto.
+ * b) quanto pagou ao INSS.
+ * c) quanto pagou ao sindicato.
+ * d) calcule os descontos e o salário líquido, conforme a tabela abaixo:
+ *
+ * + Salário Bruto: R$ - IR (11%): R$ - INSS (8%): R$ - Sindicato (5%): R$ = 
+ * Salário Líquido: R$
  */
 
 package exercicios.aula13;
