8164a24cda5c8135920e2d41f449fe016097a7a9
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-01 10:47:42 -0300

    Exercicio 10 correção

--- CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio10.java
+++ CursoJavaBasicoLoiane/src/exercicios/aula13/Exercicio10.java
@@ -10,16 +10,16 @@ public class Exercicio10 {
     
     public static void main(String[] args) {
         
-        Scanner entrada = new Scanner(System.in);
-        
         float celsius;
         float farenheit;
+        
+        Scanner entrada = new Scanner(System.in);
         
         System.out.print("Informe a temperatura em graus Celsius (°C): ");
         celsius = entrada.nextFloat();
         
         farenheit = (float) (celsius * 1.8 + 32);
         
-        System.out.printf("%.1f°C = %.1f°F%n", celsius, farenheit);
+        System.out.printf("%.1f °C = %.1f °F%n", celsius, farenheit);
     }
 }
