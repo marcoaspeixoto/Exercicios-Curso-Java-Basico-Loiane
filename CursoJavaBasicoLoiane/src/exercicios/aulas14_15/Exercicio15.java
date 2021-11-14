64564e8ac0ae667fa20f14e58d2ae7fb0e389385
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-13 23:01:29 -0300

    Exercicio15

---
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio15.java
@@ -0,0 +1,36 @@
+package exercicios.aulas14_15;
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+    
+    public static void main(String[] args) {
+        
+        int lado1, lado2, lado3;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Insira o lado 1 do triangulo: ");
+        lado1 = entrada.nextInt();
+        
+        System.out.print("Insira o lado 2 do triangulo: ");
+        lado2 = entrada.nextInt();
+        
+        System.out.print("Insira o lado 3 do triangulo: ");
+        lado3 = entrada.nextInt();
+        
+        if (lado1 + lado2 > lado3 || lado1 + lado3 > lado2 || lado2 + lado3 > lado1) {
+            if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3) {
+                System.out.println("Triângulo Equilátero");
+            } else {
+                if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+                    System.out.println("Triângulo Isósceles");
+                } else {
+                    System.out.println("Triângulo Escaleno");
+                }
+            }
+        } else {
+            System.out.println("Não é triângulo");
+        }
+    }
+}
