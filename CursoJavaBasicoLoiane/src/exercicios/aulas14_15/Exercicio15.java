ea4e68efe83c8c008ea62fa0242afd616e2b8541
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-14 11:02:33 -0300

    Exercicio15 correção da condição de triangulo

--- CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio15.java
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio15.java
@@ -19,7 +19,7 @@ public class Exercicio15 {
         System.out.print("Insira o lado 3 do triangulo: ");
         lado3 = entrada.nextInt();
         
-        if (lado1 + lado2 > lado3 || lado1 + lado3 > lado2 || lado2 + lado3 > lado1) {
+        if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2 ) {
             if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3) {
                 System.out.println("Triângulo Equilátero");
             } else {
