5a2ef03a1f0ad60856fae765af10df93929b6afb
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-16 17:04:01 -0300

    Exercicio24

---
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio24.java
@@ -0,0 +1,62 @@
+package exercicios.aulas14_15;
+
+import java.util.Scanner;
+
+public class Exercicio24 {
+
+    public static void main(String[] args) {
+
+        int numero1, numero2, resultado;
+        char operacao;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o numero 1: ");
+        numero1 = entrada.nextInt();
+
+        System.out.print("Informe o numero 2: ");
+        numero2 = entrada.nextInt();
+
+        System.out.println("+ soma");
+        System.out.println("- subtração");
+        System.out.println("* multiplicação");
+        System.out.println("/ divisão");
+        System.out.print("Informe a operação: ");
+        operacao = entrada.next().charAt(0);
+
+        if (operacao == '+') {
+            resultado = numero1 + numero2;
+            System.out.printf("%d + %d = %d%n", numero1, numero2, resultado);
+        } else {
+            if (operacao == '-') {
+                resultado = numero1 - numero2;
+                System.out.printf("%d - %d = %d%n", numero1, numero2, resultado);
+            } else {
+                if (operacao == '*') {
+                    resultado = numero1 * numero2;
+                    System.out.printf("%d * %d = %d%n", numero1, numero2, resultado);
+                } else {
+                    resultado = numero1 / numero2;
+                    System.out.printf("%d / %d = %d%n", numero1, numero2, resultado);
+                }
+            }
+        }
+
+        if (resultado > 0 || resultado < 0) {
+            if (resultado % 2 == 0) {
+                System.out.println("É par");
+            } else {
+                System.out.println("É impar");
+
+            }
+        }
+
+        if (resultado > 0) {
+            System.out.println("É positivo");
+        } else {
+            if (resultado < 0) {
+                System.out.println("É negativo");
+            }
+        }
+    }
+}
