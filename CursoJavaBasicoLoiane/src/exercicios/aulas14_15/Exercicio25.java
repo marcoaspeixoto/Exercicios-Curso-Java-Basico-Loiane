fa78cf57f0896b816cea01fdaca1facbaa986488
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-16 17:43:42 -0300

    Exercicio25

---
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio25.java
@@ -0,0 +1,67 @@
+package exercicios.aulas14_15;
+
+import java.util.Scanner;
+
+public class Exercicio25 {
+
+    public static void main(String[] args) {
+
+        int classificacao = 0;
+        char resposta;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Responda 's' para sim e 'n' para não:");
+
+        System.out.print("Telefonou para a vítima? ");
+        resposta = entrada.next().charAt(0);
+
+        if (resposta == 's') {
+            classificacao++;
+        }
+
+        System.out.print("Esteve no local do crime? ");
+        resposta = entrada.next().charAt(0);
+
+        if (resposta == 's') {
+            classificacao++;
+        }
+
+        System.out.print("Mora perto da vítima? ");
+        resposta = entrada.next().charAt(0);
+
+        if (resposta == 's') {
+            classificacao++;
+        }
+
+        System.out.print("Devia para a vítima? ");
+        resposta = entrada.next().charAt(0);
+
+        if (resposta == 's') {
+            classificacao++;
+        }
+
+        System.out.print("Já trabalhou com a vítima? ");
+        resposta = entrada.next().charAt(0);
+
+        if (resposta == 's') {
+            classificacao++;
+        }
+
+        System.out.println("");
+
+        if (classificacao == 2) {
+            System.out.println("Suspeita");
+        } else {
+            if (classificacao == 3 || classificacao == 4) {
+                System.out.println("Cúmplice");
+            } else {
+                if (classificacao == 5) {
+                    System.out.println("Assassino");
+                } else {
+                    System.out.println("Inocente");
+                }
+            }
+        }
+    }
+}
