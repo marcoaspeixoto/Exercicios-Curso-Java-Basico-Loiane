20e6785f2cfc180574bc948d357e1dbef553c08f
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-20 10:22:48 -0300

    Exercicio03 utilização de String para a variavel sexo

--- CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio03.java
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio03.java
@@ -6,17 +6,17 @@ public class Exercicio03 {
     
     public static void main(String[] args) {
         
-        char sexo;
+        String sexo;
         
         Scanner entrada = new Scanner(System.in);
         
         System.out.print("Informe o sexo (F - Feminino, M - Masculino): ");
-        sexo = entrada.next().charAt(0);
+        sexo = entrada.next();
         
-        if (sexo == 'M') {
+        if (sexo.equalsIgnoreCase("m")) {
             System.out.println("Sexo masculino");
         } else {
-            if (sexo == 'F') {
+            if (sexo.equalsIgnoreCase("f")) {
                 System.out.println("Sexo feminino");
             } else {
                 System.out.println("Sexo inválido");
