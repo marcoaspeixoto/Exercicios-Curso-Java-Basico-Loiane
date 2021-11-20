08438537ff4e9b3f7e8b9e29e3cacd14db291765
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-20 15:42:33 -0300

    Exercicio12 Adicionado na saída valor da hora x horas trabalhadas

--- CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio12.java
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio12.java
@@ -8,53 +8,53 @@ public class Exercicio12 {
         
         Scanner entrada = new Scanner(System.in);
         
-        double ganhoHora, salarioBruto, impostoDeRenda, inss, sindicato, fgts,
-                percentualIr, percentualInss, percentualSind, percentualFgts,
+        double ganhoHora, salarioBruto, impostoDeRenda, inss, sindicato, fgts, 
                 totalDescontos, salarioLiquido;
-        int horasTrabalhadas;
+        int horasTrabalhadas, percentualIr, percentualInss, percentualSind, percentualFgts;
         
         System.out.print("Informe quanto você ganha por hora: R$ ");
         ganhoHora = entrada.nextDouble();
         
-        System.out.print("Informe a quantidade de horas fechadas trabalhadas: ");
+        System.out.print("Informe a quantidade de horas fechadas trabalhadas no mês: ");
         horasTrabalhadas = entrada.nextInt();
         
         salarioBruto = ganhoHora * horasTrabalhadas;
         
         if (salarioBruto <= 900.0) {
-            percentualIr = 0.0;
+            percentualIr = 0;
         } else {
             if (salarioBruto <= 1500.0) {
-                percentualIr = 5.0;
+                percentualIr = 5;
             } else {
                 if (salarioBruto <= 2500.0) {
-                    percentualIr = 10.0;
+                    percentualIr = 10;
                 } else {
-                    percentualIr = 20.0;
+                    percentualIr = 20;
                 }
             }
         }
         
-        percentualInss = 10.0;
-        percentualSind = 3.0;
-        percentualFgts = 11.0;
+        percentualInss = 10;
+        percentualSind = 3;
+        percentualFgts = 11;
         
-        impostoDeRenda = salarioBruto * percentualIr / 100.0;
-        inss = salarioBruto * percentualInss / 100.0;
-        sindicato = salarioBruto * percentualSind / 100.0;
-        fgts = salarioBruto * percentualFgts / 100.0;
+        impostoDeRenda = salarioBruto * percentualIr / 100;
+        inss = salarioBruto * percentualInss / 100;
+        sindicato = salarioBruto * percentualSind / 100;
+        fgts = salarioBruto * percentualFgts / 100;
         totalDescontos = impostoDeRenda + inss + sindicato;
         salarioLiquido = salarioBruto - totalDescontos;
         
         System.out.println();
         
-        System.out.printf("%-25s%14s%20s%n", "DESCRIÇÃO", "PERCENTUAL (%)", "VALOR (R$)");
-        System.out.printf("%-25s%14s%,20.2f%n", "Saĺário Bruto", "", salarioBruto);
-        System.out.printf("%-25s%14.0f%,20.2f%n", "(-) Imposto de Renda", percentualIr, impostoDeRenda);
-        System.out.printf("%-25s%14.0f%,20.2f%n", "(-) INSS", percentualInss, inss);
-        System.out.printf("%-25s%14.0f%,20.2f%n", "(-) Sindicato", percentualSind, sindicato);
-        System.out.printf("%-25s%14s%,20.2f%n", "FGTS", "", fgts);
-        System.out.printf("%-25s%14s%,20.2f%n", "Total de descontos", "", totalDescontos);
-        System.out.printf("%-25s%14s%,20.2f%n", "Salário Líquido", "", salarioLiquido);
+        System.out.printf("%-30s%14s%20s%n", "DESCRIÇÃO", "PERCENTUAL (%)", "VALOR (R$)");
+        System.out.printf("%-30s%14s%,20.2f%n", "Saĺário Bruto (" + ganhoHora + " X " + 
+                horasTrabalhadas + ")", "", salarioBruto);
+        System.out.printf("%-30s%14d%,20.2f%n", "(-) Imposto de Renda", percentualIr, impostoDeRenda);
+        System.out.printf("%-30s%14d%,20.2f%n", "(-) INSS", percentualInss, inss);
+        System.out.printf("%-30s%14d%,20.2f%n", "(-) Sindicato", percentualSind, sindicato);
+        System.out.printf("%-30s%14s%,20.2f%n", "FGTS", "", fgts);
+        System.out.printf("%-30s%14s%,20.2f%n", "Total de descontos", "", totalDescontos);
+        System.out.printf("%-30s%14s%,20.2f%n", "Salário Líquido", "", salarioLiquido);
     }
 }
