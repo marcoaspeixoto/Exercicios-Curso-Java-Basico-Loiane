f623cb7a45e54cf55dd3e2b85aeee5e22ddbea77
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-20 10:54:47 -0300

    Exercicio04 Utilização de String para a variavel letra

--- CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio04.java
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio04.java
@@ -3,31 +3,35 @@ package exercicios.aulas14_15;
 import java.util.Scanner;
 
 public class Exercicio04 {
-    
+
     public static void main(String[] args) {
-        
-        char letra;
-        
+
+        String letra;
+
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.print("Informe uma letra: ");
-        letra = entrada.next().charAt(0);
-        
-        switch (letra) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'O':
-            case 'U':    
-                System.out.println("É vogal");
-                break;
-            default:
-                System.out.println("É consoante");
+        letra = entrada.next();
+
+        if (letra.length() > 1) {
+            System.out.println("Não é uma letra válida");
+        } else {
+            switch (letra) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                case "A":
+                case "E":
+                case "I":
+                case "O":
+                case "U":
+                    System.out.println("É vogal");
+                    break;
+                default:
+                    System.out.println("É consoante");
+            }
         }
     }
 }
