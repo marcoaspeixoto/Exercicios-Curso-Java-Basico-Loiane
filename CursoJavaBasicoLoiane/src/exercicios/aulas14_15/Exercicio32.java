2471407cbb494177614875f4a9a23bb743ecb547
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-20 09:37:11 -0300

    Exercicio32 mudada a lógica de acordo com o preco e adicionado comentários

--- CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio32.java
+++ CursoJavaBasicoLoiane/src/exercicios/aulas14_15/Exercicio32.java
@@ -13,22 +13,36 @@ public class Exercicio32 {
 
         System.out.print("Informe o tamanho em metros da área a ser pintada: ");
         metros = entrada.nextInt();
-
+        
+        // Guarda a quantidade de latas de 18 litros
         qtdLatas18l = metros / 108;
+        
+        /* Guarda a quantidade de latas de 3,6 litros calculaado sobre a 
+        area restante depois do cálculo das latas de 18l*/
         qtdLatas3_6l = (int) Math.ceil((metros % 108) / 21.6);
-        if (qtdLatas3_6l >= 4) {
+        
+        // Guarda o preco total das latas de 3.6 litros
+        precoTotal3_6l = qtdLatas3_6l * 25.0;
+        
+        /* Se o preço total das latas de 3,6 litros atingirem o preço da lata de
+        18 litros compensará a compra da lata de 18 litros, então é feita a substituição
+        das latas de 3,6 litros por uma lata de 18 litros*/
+        if (precoTotal3_6l >= 80.0) {
             qtdLatas18l++;
             qtdLatas3_6l = 0;
+            precoTotal3_6l = 0.0;
         }
                 
+        // Guarda o preço total das latas de 3,6 litros
         precoTotal18l = qtdLatas18l * 80.0;
-        precoTotal3_6l = qtdLatas3_6l * 25.0;
+        
+        // Guarda o valor a pagar
         valorAPagar = precoTotal18l + precoTotal3_6l;
 
-        System.out.printf("Quantidade de latas de 18 litros: %d%n", qtdLatas18l);
-        System.out.printf("Preço total das latas de 18 litros: R$ %,.2f%n", precoTotal18l);
-        System.out.printf("Quantidade de latas de 3,6 litros: %d%n", qtdLatas3_6l);
+        System.out.printf("Quantidade de latas de 18 litros...: %d%n", qtdLatas18l);
+        System.out.printf("Preço total das latas de 18 litros.: R$ %,.2f%n", precoTotal18l);
+        System.out.printf("Quantidade de latas de 3,6 litros..: %d%n", qtdLatas3_6l);
         System.out.printf("Preço total das latas de 3,6 litros: R$ %,.2f%n", precoTotal3_6l);
-        System.out.printf("Valor a pagar: R$ %,.2f%n", valorAPagar);
+        System.out.printf("Valor a pagar......................: R$ %,.2f%n", valorAPagar);
     }
 }
